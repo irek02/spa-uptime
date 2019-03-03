a388caebf65c439482ad7dde9983205630ece8f6
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2019-03-03 18:57:46 -0500

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+ 
+(async () => {
+
+  const browser = await puppeteer.launch({
+    //headless: false
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('http://codingbootcamponline.com/signup/');
+
+  const r = await page.evaluate((sel) => {
+    return document.querySelector(sel).innerHTML.search('Once enough people sign up');
+  }, 'body');
+
+  console.log(r);
+
+ // await page.pdf({path: 'hn.pdf', format: 'A4'});
+
+  await browser.close();
+
+})();
