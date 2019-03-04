03df368adc1bbe00bcca2c033670c67f7ae48b69
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2019-03-03 19:24:34 -0500

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-  const browser = puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   // const browser = await puppeteer.launch({
   //   //headless: false
   // });
